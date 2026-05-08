74d31182a10f20c5194a1410242c02e9ea857e46
chek <chek214@live.com>
2026-05-08 01:37:24 -0600

    update

--- src/views/NewUsers.vue
+++ src/views/NewUsers.vue
@@ -3,6 +3,34 @@ import { ref } from 'vue'
 
 const startDate = ref()
 const endDate = ref()
+
+const usersData = ref()
+const usersDataError = ref()
+const loading = ref(false)
+
+const organizationId = 10000
+
+async function handleReportNewUser() {
+    //startDate.value = startDate.toISOString().split('T')[0]
+    loading.value = true
+    try {
+      let response = await fetch(baseUrl + 'reports', { method: 'POST', headers: fetchHeaders, body: JSON.stringify({startDate, endDate, organizationId}) })
+      if ( response.status >= 400 ) {
+        throw new Error('Server response ' + response.status)
+      }
+      response = await response.json()
+      if ( !response.ok ) {
+        usersDataError.value = response.message
+        throw new Error(response.message)
+      }
+      usersData = response
+      loading.value = false
+    } catch (error) {
+      console.log(error)
+      loading.value = false
+    } 
+}
+
 </script>
 
 <template>
@@ -18,7 +46,7 @@ const endDate = ref()
                 <input name="endDate" v-model="endDate" type="date" id="end" />
             </div>
         </div>
-        <button  @Click="handleReportNewUser">
+        <button  @Click="handleReportNewUser()">
             Generar reporte (CSV)
         </button>
     </div>
