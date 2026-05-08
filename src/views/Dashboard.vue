0b45350cd08972b7012dc455ac9ec46b0cb3404f
chek <chek214@live.com>
2026-05-08 01:46:38 -0600

    update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,53 @@
 <script setup>
-import ActiveUsers from '@/components/ActiveUsers.vue'
+import { ref } from 'vue'
+
+const startDate = ref()
+const endDate = ref()
+
+const usersData = ref()
+const usersDataError = ref()
+const loading = ref(false)
+
+const organizationId = 10000
+
+async function handleReportNewUser() {
+    //startDate.value = startDate.toISOString().split('T')[0]
+    loading.value = true
+    try {
+      let response = await fetch(baseUrl + 'reports', { method: 'POST', headers: fetchHeaders, body: JSON.stringify({startDate, endDate, organizationId}) })
+      if ( response.status >= 400 ) {
+        throw new Error('Server response ' + response.status)
+      }
+      response = await response.json()
+      if ( !response.ok ) {
+        usersDataError.value = response.message
+        throw new Error(response.message)
+      }
+      usersData.value = response
+      loading.value = false
+    } catch (error) {
+      console.log(error)
+      loading.value = false
+    } 
+}
+
 </script>
 
 <template>
-  <main>
-  <ActiveUsers />
-  </main>
+     <div>
+        <h3>Nuevos usuarios</h3>
+        <div>
+            <div>
+                <label> Fecha inicio: {{startDate}}</label>
+                <input name="startDate" v-model="startDate" type="date" id="start" />
+            </div>
+            <div>
+                <label>Fecha fin: {{endDate}}</label>
+                <input name="endDate" v-model="endDate" type="date" id="end" />
+            </div>
+        </div>
+        <button  @Click="handleReportNewUser()">
+            Generar reporte (CSV)
+        </button>
+    </div>
 </template>
